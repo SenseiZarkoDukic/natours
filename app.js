a3de8375b54a6cb6ff53e6db9289be1eb6faff3c
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-03-24 13:19:27 +0100

    Refactor app.js to add API endpoint for retrieving tours

--- app.js
+++ app.js
@@ -2,14 +2,24 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
-});
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side!', app: 'Natours' });
+// });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint...');
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoint...');
+// });
+
+app.get('/api/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 const port = 3000;
