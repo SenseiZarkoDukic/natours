ecd37caa2feb95114f17aa65be6d776a972ac67a
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-03-25 17:25:21 +0100

    Update API response status code

--- app.js
+++ app.js
@@ -87,11 +87,33 @@ app.patch('/api/v1/tours/:id', (req, res) => {
     `${__dirname}/dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
     (err) => {
-      res.status(201).json({
+      res.status(200).json({
         status: 'success',
         data: {
           tour: editedTour,
         },
+      });
+    }
+  );
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  const deleteTour = tours.splice(tours.indexOf(tour), 1);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(204).json({
+        status: 'success',
+        data: null,
       });
     }
   );
