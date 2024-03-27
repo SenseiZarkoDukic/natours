c47358d47c02b2a68a6685e86c4c3ebc45a59fa3
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-03-27 17:19:03 +0100

    Add user controller and update user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const userController = require('../controllers/userController'); // import the user controller
+
+const { getAllUsers, getUser, createUser, updateUser, deleteUser } =
+  userController; // destructure the functions from the user controller
 
 const router = express.Router(); // create a USER router
 
