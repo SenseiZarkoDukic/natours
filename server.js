0c8d17e291b5dabaa2abe8ab261986421db8e46f
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-03-28 16:25:39 +0100

    Update config.env and package.json

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+const dotenv = require('dotenv');
 const app = require('./app');
 
-console.log(process.env.NODE_ENV);
+dotenv.config({ path: './config.env' });
+console.log(process.env);
 
 const port = 3000;
 app.listen(port, () => {
