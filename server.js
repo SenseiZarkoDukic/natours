88ee61900433161c5e98037840bc35813f53faea
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-03-31 00:32:18 +0100

    Add tourModel.js and create a test tour document

--- server.js
+++ server.js
@@ -9,13 +9,7 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD
 );
 
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log('DB connection successful!'));
+mongoose.connect(DB).then(() => console.log('DB connection successful!'));
 
 const toursSchema = new mongoose.Schema({
   name: {
@@ -34,6 +28,21 @@ const toursSchema = new mongoose.Schema({
 });
 
 const Tour = mongoose.model('Tour', toursSchema);
+
+const testTour = new Tour({
+  name: 'Hiking in the mountains of Olympus',
+  rating: 4.9,
+  price: 597,
+}); // create a new document and save it to the database
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('Error:', err);
+  }); // returns a promise
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
