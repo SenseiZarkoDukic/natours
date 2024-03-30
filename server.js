0e4e2c522c87dc5bb75954d00fda5f38ae64b821
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-03-31 00:40:32 +0100

    Add tourModel.js and update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config({ path: './config.env' });
-const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
@@ -10,39 +10,6 @@ const DB = process.env.DATABASE.replace(
 );
 
 mongoose.connect(DB).then(() => console.log('DB connection successful!'));
-
-const toursSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-});
-
-const Tour = mongoose.model('Tour', toursSchema);
-
-const testTour = new Tour({
-  name: 'Hiking in the mountains of Olympus',
-  rating: 4.9,
-  price: 597,
-}); // create a new document and save it to the database
-
-testTour
-  .save()
-  .then((doc) => {
-    console.log(doc);
-  })
-  .catch((err) => {
-    console.log('Error:', err);
-  }); // returns a promise
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
