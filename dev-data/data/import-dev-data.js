1ff62ddfc58fe0cb930a4a62cd16f189c8d463df
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-04-02 17:41:54 +0200

    Add data import functionality to dev-data module

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('./../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB).then(() => console.log('DB connection successful!'));
+
+// READ JSON FILE
+const tours = fs.readFileSync('/tours-simple.json', 'utf-8');
+
+// IMPORT DATA INTO DB
+
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded!');
+  } catch (err) {
+    console.log(err);
+  }
+};
