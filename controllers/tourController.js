6e495076235a747a8c2bce3c4d66cdab8e3caf97
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-04-04 15:29:59 +0200

    Refactor tourController.js: Update filtering and sorting logic

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -29,13 +29,13 @@ const Tour = require('./../models/tourModel');
 exports.getAllTours = async (req, res) => {
   try {
     // BUILD QUERY
-    // 1) Filtering
+    // 1A) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
     console.log(req.query, queryObj);
-    // 2) Advanced Filtering
+    // 1B) Advanced Filtering
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
     console.log(JSON.parse(queryStr));
@@ -44,7 +44,13 @@ exports.getAllTours = async (req, res) => {
     // {difficulty: 'easy', duration: {gte: '5'}}
     // gte, gt, lte, lt
 
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2) Sorting
+    if (req.query.sort) {
+      query = query.sort(req.query.sort);
+      // sort('price ratingsAverage')
+    }
 
     // EXECUTE QUERY
     const tours = await query;
