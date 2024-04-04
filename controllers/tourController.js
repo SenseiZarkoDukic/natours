031d57067b1267ff4818a0c45c96e4bbe6dabff1
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-04-04 15:08:22 +0200

    Refactor tourController.js for query filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -28,13 +28,13 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    console.log(req.query, queryObj);
     // BUILD QUERY
     // 1) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
+    console.log(req.query, queryObj);
     // 2) Advanced Filtering
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
