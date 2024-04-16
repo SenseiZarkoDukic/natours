31686f6ee60ae9ca5c1d9a3541b9de31dedaa154
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-04-16 16:17:02 +0200

    Refactor tourController.js: Update getTourStats function to include numTours and numRating fields

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -121,6 +121,8 @@ exports.getTourStats = async (req, res) => {
       {
         $group: {
           _id: null,
+          numTours: { $sum: 1 },
+          numRating: { $sum: '$ratingsQuantity' },
           avgRating: { $avg: '$ratingsAverage' },
           avgPrice: { $avg: '$price' },
           minPrice: { $min: '$price' },
