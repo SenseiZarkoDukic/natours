0ae47797d556851d5e8631ce5921835b6f9a815c
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-04-29 17:15:42 +0200

    Refactor updateTour function to use createAsync in tourController.js

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -53,25 +53,18 @@ exports.createTour = catchAsync(async (req, res) => {
   });
 });
 
-exports.updateTour = async (req, res) => {
-  try {
-    const editedTour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    res.status(200).json({
-      status: 'success',
-      data: {
-        tour: editedTour,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err.message,
-    });
-  }
-};
+exports.updateTour = createAsync(async (req, res) => {
+  const editedTour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: editedTour,
+    },
+  });
+});
 
 exports.deleteTour = async (req, res) => {
   try {
