a83d61dbb0514355e0c956bc80c80f17f3a7dc2f
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-04-29 17:14:38 +0200

    Refactor getTour function to use createAsync in tourController.js

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -30,24 +30,17 @@ exports.getAllTours = createAsync(async (req, res) => {
   });
 });
 
-exports.getTour = async (req, res) => {
-  try {
-    const tour = await Tour.findById(req.params.id);
-    // Tour.findOne({ _id: req.params.id });
+exports.getTour = createAsync(async (req, res) => {
+  const tour = await Tour.findById(req.params.id);
+  // Tour.findOne({ _id: req.params.id });
 
-    res.status(200).json({
-      status: 'success',
-      data: {
-        tour,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
 
 exports.createTour = catchAsync(async (req, res) => {
   const newTour = await Tour.create(req.body);
