3edb5022e810793f910dc87f882e63d3daebb04a
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-06-11 15:59:26 +0200

    feat: Add signup route and controller function

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const userController = require('../controllers/userController'); // import the user controller
+const authController = require('../controllers/authController'); // import the auth controller
 
 const { getAllUsers, getUser, createUser, updateUser, deleteUser } =
   userController; // destructure the functions from the user controller
 
 const router = express.Router(); // create a USER router
+
+router.post('/signup', authController.signup); // create a new user
 
 router.route('/').get(getAllUsers).post(createUser);
 
