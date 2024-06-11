29e9d5fd6cf9ecab75b2949cbbac180210bf376a
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-06-11 16:42:11 +0200

    chore: Refactor password hashing logic in user model

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   // name, email, photo, password, passwordConfirm
@@ -33,12 +34,12 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.pre('save', function (next) {
+userSchema.pre('save', async function (next) {
   // Only run this function if the password was actually modified
   if (!this.isModified('password')) return next();
 
   // Hash the password with cost of 12
-  this.password = bcrypt.hashSync(this.password, 12);
+  this.password = await bcrypt.hash(this.password, 12);
 
   // Delete the passwordConfirm field
   this.passwordConfirm = undefined;
