b325c8f7572a5f0443e045ea8fc3fc322019cfc0
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-06-12 16:45:14 +0200

    chore: Refactor createUser function to use async/await and User.create method

--- controllers/userController.js
+++ controllers/userController.js
@@ -52,53 +52,15 @@ exports.getUser = catchAsync(async (req, res) => {
   });
 });
 
-exports.createUser = (req, res) => {
-  const lastId = users[users.length - 1]._id;
-  const prefix = lastId.slice(0, -1);
-  const lastNum = parseInt(lastId.slice(-1), 10);
-  const newId = prefix + (lastNum + 1).toString();
-
-  // Split the name by spaces and take the first part
-  const firstName = req.body.name.split(' ')[0];
-  // Generate an email by replacing spaces in the name with dots and appending "@example.com"
-  const email = `${firstName.toLowerCase()}@example.com`;
-
-  // Set the role and active status
-  const role = req.body.role || 'user';
-  const active = true;
-
-  // Generate a photo name by replacing spaces in the name with dashes and appending ".jpg"
-  const photo = `user-${users.length + 1}.jpg`;
-
-  // Generate a password by hashing the name (this is just an example, you should use a more secure method in a real application)
-  const password = crypto
-    .createHash('sha256')
-    .update(req.body.name)
-    .digest('hex');
-  const newUser = Object.assign({
-    _id: newId,
-    name: req.body.name,
-    email,
-    role,
-    active,
-    photo,
-    password,
+exports.createUser = catchAsync(async (req, res) => {
+  const newUser = await User.create(req.body);
+  res.status(201).json({
+    status: 'success',
+    data: {
+      user: newUser,
+    },
   });
-  // console.log(lastId, prefix, lastNum, newId, newUser);
-  users.push(newUser);
-  fs.writeFile(
-    `${__dirname}/../dev-data/data/users.json`,
-    JSON.stringify(users),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          user: newUser,
-        },
-      });
-    }
-  );
-};
+});
 
 exports.updateUser = (req, res) => {
   const id = req.params.id;
