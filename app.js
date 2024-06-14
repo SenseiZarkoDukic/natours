0fccd114a5e4af6c46723ef6cf26b75c1f1568a1
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-06-14 16:35:34 +0200

    chore: Add logging of request headers in app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(`${__dirname}/public`)); // middleware to serve static fi
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-
+  console.log(req.headers);
   next();
 }); // custom middleware
 
