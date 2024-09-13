1a0211a18702b0f611914ea39c1d0090dcd972e9
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-09-13 02:10:43 +0200

    Refactor error handling in errorController.js and add JWT error handling

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -21,6 +21,9 @@ const handleValidationErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleJWTError = (err) =>
+  new AppError('Invalid token. Please log in again!', 401);
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
