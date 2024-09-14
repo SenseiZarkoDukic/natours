43e56e08bc8106b5a1c89f59681b556aaa97b96f
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-09-14 12:33:59 +0200

    find if user still exists in the DB

--- controllers/authController.js
+++ controllers/authController.js
@@ -73,8 +73,18 @@ exports.protect = catchAsync(async (req, res, next) => {
 
   // 2) Verification token
   const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-  console.log(decoded);
+
   // 3) Check if user still exists
+  const currentUser = await User.findById(decoded.id);
+
+  if (!currentUser) {
+    return next(
+      new AppError(
+        'The user belonging to this token does no longer exist.',
+        401
+      )
+    );
+  }
 
   // 4) Check if user changed password after the token was issued
   next();
