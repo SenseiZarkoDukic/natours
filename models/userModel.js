923598dc2e444ee2f85851d0d89c9bf53b987229
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-09-16 17:08:15 +0200

    Refactor password change check in userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -59,7 +59,6 @@ userSchema.methods.correctPassword = async function (
 
 userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
   if (this.passwordChangedAt) {
-    console.log(this.passwordChangedAt, JWTTimestamp);
     const changedTimestamp = parseInt(
       this.passwordChangedAt.getTime() / 1000,
       10
