8e77a70ccb1ee8be66117e702d7abbd42c101544
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-09-18 17:50:43 +0200

    Add nodemailer package for email functionality

---
+++ utils/email.js
@@ -0,0 +1,10 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async (options) => {
+  // 1) Create a transporter
+  const transporter = nodemailer.createTransport({});
+
+  // 2) Define the email options
+
+  // 3) Actually send the email
+};
