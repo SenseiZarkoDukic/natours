0282ee83b578413de29d80acd628577fb0543c76
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-09-19 16:48:25 +0200

    Refactor authController.js to add updatePassword function and update the user's password

--- controllers/authController.js
+++ controllers/authController.js
@@ -193,7 +193,18 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
 
 exports.updatePassword = catchAsync(async (req, res, next) => {
   // 1) Get user from collection
+  const user = await User.findById(req.user.id).select('+password');
   // 2) Check if POSTed current password is correct
+  if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
+    return next(new AppError('Your current password is wrong.', 401));
+  }
   // 3) If so, update password
+  user.password = req.body.password;
   // 4) Log user in, send JWT
+  const token = signToken(user._id);
+
+  res.status(200).json({
+    status: 'success',
+    token,
+  });
 });
